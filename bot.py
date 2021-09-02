c287e1bc6695683ea9385ec4674811cc68447d06
Dennis <blinhic850@mail.ru>
2021-09-02 13:45:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,6 +23,22 @@ data_loads3 = json.load(open('./schedule_next_day.json'))
 data3 = json.dumps(data_loads3)
 json_data3 = json.loads(data3)
 
+api_url = "https://qnext.app/bin/webhooks/6438/181/4opQGmB6OlHqqPRM"
+bot_id = os.environ.get('bot_id')
+api_token = os.environ.get('bot_api')
+
+def send_event(user_id, event_name):
+    data = {
+        "botId": bot_id,
+        "apiToken": api_token,
+        "requestType": "event",
+        "userId": user_id,
+        "event": "/start"
+    }
+
+    result = requests.post(api_url, data)
+    print(result.text)
+
 @bot.message_handler(commands = ['start'])
 def start_command(message):
     str_countes = ''
